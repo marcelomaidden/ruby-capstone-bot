a3113e38406eface6cc3739850178044bd318211
Marcelo Fernandes de Araujo <marcelomaidden@gmail.com>
2020-11-06 14:14:18 -0300

    Add public_channels and conversation_history methods

--- bin/run.rb
+++ bin/run.rb
@@ -1,2 +1,9 @@
 require_relative '../lib/bot'
 
+bot = Bot.new
+
+public_channels = bot.public_channels
+
+public_channels["channels"].each { |k, v| puts "#{k}:#{v}\n"}
+
+puts bot.conversation_history('C01E4JG6LG2')
--- lib/bot.rb
+++ lib/bot.rb
@@ -3,11 +3,31 @@ require 'httparty'
 require_relative 'token'
 
 class Bot
+  # HTTParty is used to request a url via web api
   include HTTParty
+  # Auth module to retrieve token
   extend Auth
+  
+  # parameteres used by HTTParty
   default_params token: show_token
+  format :json
+
+  def request(url, query=nil)
+    @response = Bot.get(url)
+  end
+
+  def response
+    @response
+  end
+
+  def public_channels
+    return request('https://slack.com/api/conversations.list')
+  end
+
+  def conversation_history(channel)
+    return Bot.get('https://slack.com/api/conversations.history', query: {channel: channel})
+  end
 end
 
-puts Bot.get('https://slack.com/api/conversations.list')
 
 
