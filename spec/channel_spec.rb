8706bf55a1203f58c7ff011e0beca76ba8b676f4
Marcelo Fernandes de Araujo <marcelomaidden@gmail.com>
2020-11-07 20:56:42 -0300

    Add raise error test for channel

--- spec/channel_spec.rb
+++ spec/channel_spec.rb
@@ -2,10 +2,13 @@ require_relative '../lib/channel'
 
 describe Channel do
   let(:channel) { Channel.new(1, 'Test', 'topic', 'purpose') }
+  let(:channel2) { Channel.new }
 
   describe '#initialize' do
     context 'Checks creation for an instance of Channel' do
       it { expect(channel).to be_an_instance_of(Channel) }
+
+      it { expect{channel2}.to raise_error(ArgumentError) }
 
       it { expect(channel).not_to be_an_instance_of(Integer) }
 
