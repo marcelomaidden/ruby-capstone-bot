2c8a4b0520491d54eba1cec3a444a75ff53edad1
Marcelo Fernandes de Araujo <marcelomaidden@gmail.com>
2020-11-06 21:03:58 -0300

    Add messages on run.rb file

--- bin/run.rb
+++ bin/run.rb
@@ -2,7 +2,10 @@ require_relative '../lib/bot'
 
 bot = Bot.new
 
+puts "Starting bot\n"
+
 loop do
+  puts "\nReading messages\n"
   bot.channels.each do |k, _ch|
     bot.conversation_history(k)
   end
